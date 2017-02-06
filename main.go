505c0690454079857bcc688d61551567533de1f3
Groovilator <Sajak@Tylers-Air.lan>
2017-02-06 07:25:05 -0700

    Comments
    Added comments for further documentation

--- main.go
+++ main.go
@@ -10,11 +10,13 @@ import (
 	"time"
 )
 
+// Listener wrapper to hold server state information
 type GracefulListener struct {
 	net.Listener
 	wg *sync.WaitGroup
 }
 
+// Conn wrapper to hold server state information
 type GracefulConn struct {
 	net.Conn
 	wg *sync.WaitGroup
@@ -28,6 +30,8 @@ func NewGracefulConn(wg *sync.WaitGroup, conn net.Conn) *GracefulConn {
 	return &GracefulConn{Conn: conn, wg: wg}
 }
 
+// Listener.Accept() implementation that add's to the server's waitgroup if a 
+// connection is successful created
 func (gl *GracefulListener) Accept() (net.Conn, error) {
 	conn, err := gl.Listener.Accept()
 	if err == nil {
@@ -36,6 +40,8 @@ func (gl *GracefulListener) Accept() (net.Conn, error) {
 	return NewGracefulConn(gl.wg, conn), err
 }
 
+// Conn.Close() implemenation that decrements the server's waitgroup when a 
+// connection is closed d
 func (gconn *GracefulConn) Close() error {
 	gconn.wg.Done()
 	return gconn.Conn.Close()
@@ -44,9 +50,11 @@ func (gconn *GracefulConn) Close() error {
 func hashHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received hash request")
 
+	// parse "password" parameter, pass = "" if not specified
 	r.ParseForm()
 	pass := r.Form.Get("password")
 
+	// block for 5 seconds
 	time.Sleep(5 * time.Second)
 
 	sha_512 := sha512.New()
@@ -67,13 +75,15 @@ func main() {
 	http.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("*Shutdown Started*")
 		fmt.Printf("Closing listener\n")
+		// Close listener (in turn stopping the server)
 		gl.Close()
 	})
 	server := http.Server{}
 
 	fmt.Println("*Serving HTTP*")
 	server.Serve(gl)
-
+            
 	fmt.Println("Waiting on pending responses")
 	gl.wg.Wait()
 }
+
